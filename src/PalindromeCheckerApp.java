918b8e54da9c4fe7b9ca1f03d0aedc79018969b4
Abi-12-08 <abi.viji8006@gmail.com>
2026-03-13 15:34:22 +0530

    UC8: Linked list based palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,33 +1,67 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+class Node {
+    char data;
+    Node next;
 
-public class UseCase7PalindromeCheckerApp {
+    Node(char data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class UseCase8PalindromeCheckerApp {
+
+    static Node head = null;
+
+    static void insert(char c){
+
+        Node newNode = new Node(c);
+
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        while(temp.next != null){
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    static boolean isPalindrome(){
+
+        String original = "";
+        String reversed = "";
+
+        Node temp = head;
+
+        while(temp != null){
+            original += temp.data;
+            temp = temp.next;
+        }
+
+        for(int i = original.length()-1; i >=0; i--){
+            reversed += original.charAt(i);
+        }
+
+        return original.equals(reversed);
+    }
 
     public static void main(String[] args) {
 
-        String word = "radar";
-
-        Deque<Character> deque = new ArrayDeque<>();
+        String word = "level";
 
         for(char c : word.toCharArray()){
-            deque.addLast(c);
+            insert(c);
         }
 
-        boolean isPalindrome = true;
-
-        while(deque.size() > 1){
-
-            if(deque.removeFirst() != deque.removeLast()){
-                isPalindrome = false;
-                break;
-            }
-
-        }
-
-        if(isPalindrome){
+        if(isPalindrome()){
             System.out.println(word + " is a palindrome");
         }else{
             System.out.println(word + " is not a palindrome");
         }
+
     }
 }
