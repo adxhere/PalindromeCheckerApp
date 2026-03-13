4d7801a6658fca3460a4a5f622564a6e9d704fb3
adxhere <jaiaaditya2@gmail.com>
2026-03-13 08:31:28 +0530

    UC3: Palindrome using reverse string

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,11 +2,22 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Version : 1.0");
-        System.out.println("=================================");
+        String word = "level";
+        String reversed = "";
+
+        for(int i = word.length() - 1; i >= 0; i--){
+            reversed = reversed + word.charAt(i);
+        }
+
+        if(word.equals(reversed)){
+            System.out.println(word + " is a palindrome");
+        }
+        else{
+            System.out.println(word + " is not a palindrome");
+        }
 
     }
+
+}
 
 }
